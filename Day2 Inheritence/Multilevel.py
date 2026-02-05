e47334fe08db80c8812e0b9b9db08c469bd85210
Rohit4Rohi <tnrohit6@gmail.com>
2026-02-05 13:26:10 +0530

    multilevel updated

--- Day2 Inheritence/Multilevel.py
+++ Day2 Inheritence/Multilevel.py
@@ -59,6 +59,42 @@ mp.display_electronic_product()
 mp.display_product()      
 
 
+# Another program for Multilevel inheritance
+
+class grand_father:
+    def __init__(self,property):
+        self.grand_father_property=property
+
+    def display_property1(self):
+        print("grands father Property is:",self.grand_father_property)
+
+
+class father(grand_father):
+    def __init__(self,property,g_f_property):
+        self.father_property=property
+        super().__init__(g_f_property)
+
+    def display_property2(self):
+        print("Father's Property is:",self.father_property)    
+        
+        
+
+class son(father):
+    def __init__(self,property,f_property,g_f_property):
+        self.property=property
+        super().__init__(f_property,g_f_property)
+        
+
+    def display_property3(self):
+        print("Son's Property is:",self.property)
+
+child_obj=son("house","car","bike")
+child_obj.display_property3()
+child_obj.display_property2()
+child_obj.display_property1()
+
+
+
 
 
 

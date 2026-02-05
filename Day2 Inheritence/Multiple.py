5d28016e9ee1ebb10a3f6704a2670b6584225227
Rohit4Rohi <tnrohit6@gmail.com>
2026-02-05 12:32:42 +0530

    updated multiple

--- Day2 Inheritence/Multiple.py
+++ Day2 Inheritence/Multiple.py
@@ -49,7 +49,48 @@ sp.display_camera_details()
 sp.display_music_details()    
 
 
+# Another program for Multiple inheritance
 
+class father:
+    def __init__(self,surname,name):
+        self.surname=surname
+        self.father_name=name
+
+    def display_surname(self):
+        print("Surname is:",self.surname)
+    def display_father_name(self):
+        print("the father is",self.father_name)
+
+
+class mother():
+    def __init__(self,eye_color,name):
+        self.mother_name=name
+        self.eye_color=eye_color
+        # super().__init__(surname,father_name)
+
+    def display_eye_color(self):
+        print("the eye color is",self.eye_color)    
+    def display_mother_name(self):
+        print("the mother is",self.mother_name)        
+        
+
+class son(mother,father):
+    def __init__(self,name,father_name,surname,mother_name,eye_color):
+        self.name=name
+        father.__init__(self,surname,father_name)
+        mother.__init__(self,eye_color,mother_name)
+        # super().__init__(surname,father_name,eye_color,mother_name)
+
+    def display_name(self):
+        print("Name is:",self.name)
+        
+
+child_obj=son("Rohith","Nagaraj","T","suchithra","black")
+child_obj.display_father_name()
+child_obj.display_surname()
+child_obj.display_mother_name()
+child_obj.display_eye_color()
+child_obj.display_name()
 
 
 
